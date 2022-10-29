3a898b4101f89497681fda0eccc4a263b93e251c
Mike Schwörer <pubgit@mikescher.com>
2022-10-29 14:31:57 +0200

    Added --data-only flag to `ffsclient get`

--- cli/impl/cliRecordsGet.go
+++ cli/impl/cliRecordsGet.go
@@ -16,6 +16,7 @@ type CLIArgumentsRecordsGet struct {
 	Raw         bool
 	Decoded     bool
 	PrettyPrint bool
+	DataOnly    bool
 
 	CLIArgumentsRecordsUtil
 }
@@ -25,6 +26,7 @@ func NewCLIArgumentsRecordsGet() *CLIArgumentsRecordsGet {
 		Raw:         false,
 		Decoded:     false,
 		PrettyPrint: false,
+		DataOnly:    false,
 	}
 }
 
@@ -45,17 +47,20 @@ func (a *CLIArgumentsRecordsGet) ShortHelp() [][]string {
 		{"ffsclient get <collection> <record-id>", "Get a single record"},
 		{"          (--raw | --decoded)", "Return raw data or decoded payload"},
 		{"          [--pretty-print | --pp]", "Pretty-Print json in decoded data / payload (if possible)"},
+		{"          [--data-only]", "Only return the payload"},
 	}
 }
 
 func (a *CLIArgumentsRecordsGet) FullHelp() []string {
 	return []string{
-		"$> ffsclient get <collection> <record-id> (--raw | --decoded) [--pretty-print | --pp]",
+		"$> ffsclient get <collection> <record-id> (--raw | --decoded) [--pretty-print | --pp] [--data-only]",
 		"",
 		"Get data of a single record",
 		"",
 		"Either --raw or --decoded must be specified",
 		"If --pretty-print is specified we try to pretty-print the payload/data, only works if it is in JSON format",
+		"By default we return the 'envelope' around the payload with meta information like id and modification-data.",
+		"Use --data-only to only return the (decoded or raw) payload",
 	}
 }
 
@@ -74,6 +79,10 @@ func (a *CLIArgumentsRecordsGet) Init(positionalArgs []string, optionArgs []cli.
 		}
 		if (arg.Key == "pretty-print" || arg.Key == "pp") && arg.Value == nil {
 			a.PrettyPrint = true
+			continue
+		}
+		if (arg.Key == "data-only") && arg.Value == nil {
+			a.DataOnly = true
 			continue
 		}
 		return fferr.DirectOutput.New("Unknown argument: " + arg.Key)
@@ -129,41 +138,58 @@ func (a *CLIArgumentsRecordsGet) printRaw(ctx *cli.FFSContext, v models.Record) 
 	switch langext.Coalesce(ctx.Opt.Format, cli.OutputFormatText) {
 
 	case cli.OutputFormatText:
-		ctx.PrintPrimaryOutput(v.ID)
-		ctx.PrintPrimaryOutput(v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano))
-		ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, v.Payload, false))
-		ctx.PrintPrimaryOutput("")
+		if a.DataOnly {
+			ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, v.Payload, false))
+			ctx.PrintPrimaryOutput("")
+		} else {
+			ctx.PrintPrimaryOutput(v.ID)
+			ctx.PrintPrimaryOutput(v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano))
+			ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, v.Payload, false))
+			ctx.PrintPrimaryOutput("")
+		}
 		return nil
 
 	case cli.OutputFormatJson:
-		ctx.PrintPrimaryOutputJSON(langext.H{
-			"id":            v.ID,
-			"ttl":           v.TTL,
-			"sortIndex":     v.SortIndex,
-			"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
-			"modified_unix": v.ModifiedUnix,
-			"payload":       v.Payload,
-		})
+		if a.DataOnly {
+			ctx.PrintPrimaryOutputJSON(langext.H{"payload": v.Payload})
+		} else {
+			ctx.PrintPrimaryOutputJSON(langext.H{
+				"id":            v.ID,
+				"ttl":           v.TTL,
+				"sortIndex":     v.SortIndex,
+				"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
+				"modified_unix": v.ModifiedUnix,
+				"payload":       v.Payload,
+			})
+		}
 		return nil
 
 	case cli.OutputFormatXML:
-		type xml struct {
-			ID           string   `xml:"ID,attr"`
-			TTL          string   `xml:"TTL,attr,omitempty"`
-			SortIndex    int64    `xml:"SortIndex,attr"`
-			Modified     string   `xml:"Modified,attr"`
-			ModifiedUnix float64  `xml:"ModifiedUnix,attr"`
-			Payload      string   `xml:",chardata"`
-			XMLName      struct{} `xml:"Record"`
+		if a.DataOnly {
+			type xml struct {
+				Payload string   `xml:",chardata"`
+				XMLName struct{} `xml:"Data"`
+			}
+			ctx.PrintPrimaryOutputXML(xml{Payload: a.prettyPrint(ctx, a.PrettyPrint, v.Payload, true)})
+		} else {
+			type xml struct {
+				ID           string   `xml:"ID,attr"`
+				TTL          string   `xml:"TTL,attr,omitempty"`
+				SortIndex    int64    `xml:"SortIndex,attr"`
+				Modified     string   `xml:"Modified,attr"`
+				ModifiedUnix float64  `xml:"ModifiedUnix,attr"`
+				Payload      string   `xml:",chardata"`
+				XMLName      struct{} `xml:"Record"`
+			}
+			ctx.PrintPrimaryOutputXML(xml{
+				ID:           v.ID,
+				TTL:          langext.NumToStringOpt(v.TTL, ""),
+				SortIndex:    v.SortIndex,
+				Modified:     v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
+				ModifiedUnix: v.ModifiedUnix,
+				Payload:      a.prettyPrint(ctx, a.PrettyPrint, v.Payload, true),
+			})
 		}
-		ctx.PrintPrimaryOutputXML(xml{
-			ID:           v.ID,
-			TTL:          langext.NumToStringOpt(v.TTL, ""),
-			SortIndex:    v.SortIndex,
-			Modified:     v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
-			ModifiedUnix: v.ModifiedUnix,
-			Payload:      a.prettyPrint(ctx, a.PrettyPrint, v.Payload, true),
-		})
 		return nil
 
 	default:
@@ -176,52 +202,75 @@ func (a *CLIArgumentsRecordsGet) printDecoded(ctx *cli.FFSContext, v models.Reco
 	switch langext.Coalesce(ctx.Opt.Format, cli.OutputFormatText) {
 
 	case cli.OutputFormatText:
-		ctx.PrintPrimaryOutput(v.ID)
-		ctx.PrintPrimaryOutput(v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano))
-		ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), false))
-		ctx.PrintPrimaryOutput("")
+		if a.DataOnly {
+			ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), false))
+			ctx.PrintPrimaryOutput("")
+		} else {
+			ctx.PrintPrimaryOutput(v.ID)
+			ctx.PrintPrimaryOutput(v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano))
+			ctx.PrintPrimaryOutput(a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), false))
+			ctx.PrintPrimaryOutput("")
+		}
 		return nil
 
 	case cli.OutputFormatJson:
-		if a.PrettyPrint {
-			ctx.PrintPrimaryOutputJSON(langext.H{
-				"id":            v.ID,
-				"ttl":           v.TTL,
-				"sortIndex":     v.SortIndex,
-				"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
-				"modified_unix": v.ModifiedUnix,
-				"data":          a.tryParseJson(ctx, v.DecodedData),
-			})
+		if a.DataOnly {
+			if a.PrettyPrint {
+				ctx.PrintPrimaryOutputJSON(langext.H{"data": a.tryParseJson(ctx, v.DecodedData)})
+			} else {
+				ctx.PrintPrimaryOutputJSON(langext.H{"data": string(v.DecodedData)})
+			}
 		} else {
-			ctx.PrintPrimaryOutputJSON(langext.H{
-				"id":            v.ID,
-				"ttl":           v.TTL,
-				"sortIndex":     v.SortIndex,
-				"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
-				"modified_unix": v.ModifiedUnix,
-				"data":          string(v.DecodedData),
-			})
+			if a.PrettyPrint {
+				ctx.PrintPrimaryOutputJSON(langext.H{
+					"id":            v.ID,
+					"ttl":           v.TTL,
+					"sortIndex":     v.SortIndex,
+					"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
+					"modified_unix": v.ModifiedUnix,
+					"data":          a.tryParseJson(ctx, v.DecodedData),
+				})
+			} else {
+				ctx.PrintPrimaryOutputJSON(langext.H{
+					"id":            v.ID,
+					"ttl":           v.TTL,
+					"sortIndex":     v.SortIndex,
+					"modified":      v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
+					"modified_unix": v.ModifiedUnix,
+					"data":          string(v.DecodedData),
+				})
+			}
 		}
 		return nil
 
 	case cli.OutputFormatXML:
-		type xml struct {
-			ID           string   `xml:"ID,attr"`
-			TTL          string   `xml:"TTL,attr,omitempty"`
-			SortIndex    int64    `xml:"SortIndex,attr"`
-			Modified     string   `xml:"Modified,attr"`
-			ModifiedUnix float64  `xml:"ModifiedUnix,attr"`
-			Data         string   `xml:",chardata"`
-			XMLName      struct{} `xml:"Record"`
+		if a.DataOnly {
+			type xml struct {
+				Data    string   `xml:",chardata"`
+				XMLName struct{} `xml:"Record"`
+			}
+			ctx.PrintPrimaryOutputXML(xml{
+				Data: a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), true),
+			})
+		} else {
+			type xml struct {
+				ID           string   `xml:"ID,attr"`
+				TTL          string   `xml:"TTL,attr,omitempty"`
+				SortIndex    int64    `xml:"SortIndex,attr"`
+				Modified     string   `xml:"Modified,attr"`
+				ModifiedUnix float64  `xml:"ModifiedUnix,attr"`
+				Data         string   `xml:",chardata"`
+				XMLName      struct{} `xml:"Record"`
+			}
+			ctx.PrintPrimaryOutputXML(xml{
+				ID:           v.ID,
+				TTL:          langext.NumToStringOpt(v.TTL, ""),
+				SortIndex:    v.SortIndex,
+				Modified:     v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
+				ModifiedUnix: v.ModifiedUnix,
+				Data:         a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), true),
+			})
 		}
-		ctx.PrintPrimaryOutputXML(xml{
-			ID:           v.ID,
-			TTL:          langext.NumToStringOpt(v.TTL, ""),
-			SortIndex:    v.SortIndex,
-			Modified:     v.Modified.In(ctx.Opt.TimeZone).Format(time.RFC3339Nano),
-			ModifiedUnix: v.ModifiedUnix,
-			Data:         a.prettyPrint(ctx, a.PrettyPrint, string(v.DecodedData), true),
-		})
 		return nil
 
 	default:
